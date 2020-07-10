59d47de54a025ccd54384bcdada17c59aa253d3d
Ahmad Awais <me@AhmadAwais.com>
2020-07-11 02:54:13 +0500

    👌 IMPROVE: Test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const scarfPackage = require('.');
 
 (async () => {
-	const pkg = {name: `cli-meow-help`};
+	const pkg = {name: `npm-pkg-name`};
 	const res = await scarfPackage(pkg);
 	if (res.status === 200) {
 		console.log(`SUCCESS: ${pkg.name} added!`);
